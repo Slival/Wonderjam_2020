23062ae1e0a2e3a93b7705a050f7400789a0eb61
julienbil <julienbil38@live.fr>
2020-10-09 15:50:59 -0400

    Text to speech working
    aeiou

---
+++ Assets/TTS/SpellCasting.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using SpeechLib;
+using System.Xml;
+using System.IO;
+
+public class SpellCasting : MonoBehaviour
+{
+    TMP_InputField input;
+    bool isTyping = false;
+    private SpVoice voice;
+
+    string loadXMLStandalone(string fileName)
+    {
+
+        string path = Path.Combine("Resources", fileName);
+        path = Path.Combine(Application.dataPath, path);
+        Debug.Log("Path:  " + path);
+        StreamReader streamReader = new StreamReader(path);
+        string streamString = streamReader.ReadToEnd();
+        Debug.Log("STREAM XML STRING: " + streamString);
+        return streamString;
+    }
+
+    private void Start()
+    {
+        voice = new SpVoice();
+        input = GetComponent<TMP_InputField>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Return) && !isTyping)
+        {
+            input.ActivateInputField();
+            input.Select();
+            isTyping = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Return) && isTyping)
+        {
+            voice.Speak(input.text, SpeechVoiceSpeakFlags.SVSFlagsAsync | SpeechVoiceSpeakFlags.SVSFIsXML);
+            input.text = "";
+            isTyping = false;
+        }
+    }
+
+
+}
