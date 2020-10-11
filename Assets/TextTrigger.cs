be8fca13afacf77c67d549b9ae8fa62b42ac5add
julienbil <julienbil38@live.fr>
2020-10-10 21:45:54 -0400

    Textbox + moth moment

---
+++ Assets/TextTrigger.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextTrigger : MonoBehaviour
+{
+    public GameObject textbox;
+    public string text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            textbox.SetActive(true);
+            textbox.GetComponent<typeText>().StartText(text);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            textbox.SetActive(false);
+        }
+    }
+}
